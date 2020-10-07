9d5d383a07489122b7bd74c04c2cf7cd1a60d07b
Adhyayan Panwar <nullvoiddeath@gmail.com>
2020-10-07 15:04:39 +0530

    07/10/2020

---
+++ Lab3/Lab3.r
@@ -0,0 +1,9 @@
+buff_tail <- c(10, 1, 37, 5, 12)
+garden_bee <- c(8, 3, 9, 6, 4)
+red_tail <- c(18, 9, 12, 4, 8)
+carder_bee <- c(8, 27, 6, 32, 230)
+honey_bee <- c(12, 13, 16, 9, 10)
+bee_data_frame <- data.frame(buff_tail, garden_bee, red_tail, carder_bee, honey_bee)
+bee_data_frame
+rownames(bee_data_frame) <- c("Thistle","Vipers","Golden Rain","Yell","Green")
+bee_data_frame
