db35e29d281718bd542c41bf2a67d54ec88a0873
Adhyayan Panwar <nullvoiddeath@gmail.com>
2020-10-28 15:00:09 +0530

    28/10/2020

--- Lab 05/5b.r
+++ Lab 05/5b.r
@@ -6,11 +6,5 @@ y<-rnorm(100,mean=0,sd=1)
 y
 dat<-as.data.frame(cbind(x,y))
 boxplot(dat, col="blue")
-
-x<-rnorm(100,mean=0,sd=1)
-x
-y<-rnorm(100,mean=0,sd=1)
-y
-dat<-as.data.frame(cbind(x,y))
 ggplot()+geom_point(data=dat, aes(x=x,y=y),size=3, color="blue")+ggtitle("Scatter Plot")+labs(y= "Y Axis", x = "X Axis")
 
