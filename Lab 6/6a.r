ef16620cacf92d02f2c3b4900fab335b1f7b3a27
Adhyayan Panwar <nullvoiddeath@gmail.com>
2020-11-04 15:20:07 +0530

    04/11/2020

--- Lab 6/6a.r
+++ Lab 6/6a.r
@@ -1,2 +1,4 @@
-library("gcookbook")
-barplot(Weight ~ Cultivar+Date, data=cabbage_exp, xlab = "Date", ylab = "Weight", beside=TRUE, legend = TRUE, col=c("red3", "grey"))
+library(gcookbook)
+library(ggplot2)
+ggplot(cabbage_exp, aes(x=Date, y=Weight, fill=Cultivar)) +geom_bar(stat="identity", position="dodge") +geom_text(aes(label=Weight), vjust=1.5, colour="white",
+                                                                                                                  position=position_dodge(.9), size=3)
