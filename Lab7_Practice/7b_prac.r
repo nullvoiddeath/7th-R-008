25c0d63af0f580580a5b90c365e089f1624c6614
Adhyayan Panwar <nullvoiddeath@gmail.com>
2020-11-18 15:03:23 +0530

    18/11/2020

--- Lab7_Practice/7b_prac.r
+++ Lab7_Practice/7b_prac.r
@@ -6,4 +6,4 @@ qnorm(0.05)
 visualize.norm(stat=zvalue, mu=0, sd=1,section="upper")
 
 library(BSDA)
-z.test(x=D$Machine.1, alternative="greater", sigma, x=2,mu=150)
+z.test(x=D$Machine.1, alternative="greater", sigma.x=2,mu=150)
